ff7146dd915647411ff4264a06228cff104ab68d
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-03-06 23:02:27 +0500

    update

---
+++ hover effects/hover hexagon bg/js/app.js
@@ -0,0 +1,18 @@
+let kub26 = document.querySelectorAll(".wrap_flex")
+
+for (let k = 0; k < kub26.length; k++) {
+    for (let i = 0; i < 10; i++) {
+    let three2 = document.createElement("div");
+    three2.classList ="kub"
+    kub26[k].append(three2)
+}
+}
+ 
+let kub25 = document.querySelectorAll(".wrap_flex1")
+for (let k = 0; k < kub25.length; k++) {
+    for (let i = 0; i < 10; i++) {
+    let three1 = document.createElement("div");
+    three1.classList ="kub"
+    kub25[k].append(three1)
+}
+}
